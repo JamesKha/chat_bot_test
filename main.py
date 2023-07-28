93d4c1c3a8975cc3f9f8ee5ce492034ad3093fe1
JamesKha <jimmy.kha@mytwu.ca>
2023-07-27 22:39:33 -0700

    added content

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
-import streamlit as st 
-from streamlit_chat import message 
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
-import os 
+import os
 
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from streamlit_chat import message
+import openai
 
 def main():
-    if os.getenv("OPEN_API_KEY") is None or os.getenv("OPEN_API_KEY"):
-        print("Key is not set")
-        exit(1)
-    else:
-        print("Key is set")
 
     chat = ChatOpenAI()
 
-    messages = SystemMessage(content="You are a helpful assistant.")
+    messages = [SystemMessage(content="You are a helpful assistant.")]
 
     with st.sidebar:
         user_input =  st.text_input("You message: ", key = "user_input")
 
     if user_input:
         message(user_input, is_user=True)
-        messages.append(HumanMessage(content=user_input))
-        response = chat(message)
+        st.session_state.messages.append(HumanMessage(conten=user_input))
+        with st.spinner("Thinking"):
+            response=chat(st.session_state.messages)
+        st.session_state.append(AIMessage(content=response.content))
         message(response.content, is_user=False)
 
+
+    messages = st.session_state.get('messages', [])
+    for i, msg in enumerate(messages):
+        if i % 2 == 0:
+            message(msg.content, is_user=True, key=str(i) + '_user')
+        else:
+            message(msg.content, is_user=False, key=str(i) + '_AI')
 if __name__ == '__main__':
     main()
