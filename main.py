ebfe3bb19c7f87fc0e739b136371d281c838710c
JamesKha <jimmy.kha@mytwu.ca>
2023-07-28 07:28:04 -0700

    langchain add

--- main.py
+++ main.py
@@ -4,23 +4,30 @@ import streamlit as st
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from streamlit_chat import message
-import openai
+from langchain.agents import load_tools, initialize_agent, AgentType
+from langchain.sql_database import SQLDatabase
+from langchain.embeddings import OpenAIEmbeddings
 
 def main():
 
     chat = ChatOpenAI()
+    
 
     messages = [SystemMessage(content="You are a helpful assistant.")]
 
+    if "messages" not in st.session_state:
+        st.session_state.messages = [SystemMessage(content="You are a helpful assistant. ")]
+    
+    
     with st.sidebar:
         user_input =  st.text_input("You message: ", key = "user_input")
 
     if user_input:
         message(user_input, is_user=True)
-        st.session_state.messages.append(HumanMessage(conten=user_input))
+        st.session_state.messages.append(HumanMessage(content=user_input))
         with st.spinner("Thinking"):
             response=chat(st.session_state.messages)
-        st.session_state.append(AIMessage(content=response.content))
+        st.session_state.messages.append(AIMessage(content=response.content))
         message(response.content, is_user=False)
 
 
